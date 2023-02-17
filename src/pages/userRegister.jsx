2265c30b9aa1b521771daa53625cbc9f9d9cc75f
Rafael Cabral <rafaelcabral261992@gmail.com>
2023-02-17 15:00:34 -0300

    add:flowbite

--- src/pages/userRegister.jsx
+++ src/pages/userRegister.jsx
@@ -34,43 +34,39 @@ const submitUserData = (e) => {
 
 return ( 
   <div className="flex justify-center">
-     <form  className=" my-52  flex flex-col border-2 rounded-xl bg-blue-100" >
+     <form  className=" flex flex-col border-2 rounded-xl my-20 w-96 shadow-2xl" >
   <div className="flex flex-col m-auto p-10">
-    <h1>Cadastro</h1>
+    <h1 className="self-center text-slate-500 text-3xl">Cadastro</h1>
     <hr/>
 
-    <label htmlFor="nome">Nome</label>
-    <input type="text" placeholder="Nome" name="nome" id="nome"  onChange={handleData} required/>
+    <label className="m-1" htmlFor="nome">Nome</label>
+    <input className="input-field" type="text" placeholder="Nome" name="nome" id="nome"  onChange={handleData} required/>
 
-    <label htmlFor="email">Email</label>
-    <input type="text" placeholder="email@email.com" name="email" id="email"  onChange={handleData} required/>
+    <label className="m-1" htmlFor="email">Email</label>
+    <input className="input-field" type="text" placeholder="email@email.com" name="email" id="email"  onChange={handleData} required/>
 
-    <label htmlFor="password">Senha</label>
-    <input type="password" placeholder="********" name="password" id="password"  onChange={handleData}  required/>
+    <label className="m-1" htmlFor="password">Senha</label>
+    <input className="input-field" type="password" placeholder="********" name="password" id="password"  onChange={handleData}  required/>
 
-    <label htmlFor="confirm-password">Confirmar Senha</label>
-    <input type="password" placeholder="********" name="confirm-password" id="confirm-password" required/>
-    <hr/>
-    <label htmlFor="data_nasc">Data de Nascimento</label>
-    <input type="date" name="data_nasc" id="data_nasc" onChange={handleData} required/>
-    <hr/>
+    <label className="m-1" htmlFor="confirm-password">Confirmar Senha</label>
+    <input className="input-field" type="password" placeholder="********" name="confirm-password" id="confirm-password" required/>
+    
+    <label className="m-1" htmlFor="data_nasc">Data de Nascimento</label>
+    <input className="input-field" type="date" name="data_nasc" id="data_nasc" onChange={handleData} required/>
 
-    <label htmlFor="cpf">CPF</label>
-    <input type="number" name="cpf" id="cpf"  onChange={handleData} required/>
-    <hr/>
+    <label className="m-1" htmlFor="cpf">CPF</label>
+    <input className="input-field " type="number" name="cpf" id="cpf"  onChange={handleData} required/>
 
-    <label htmlFor="telefone">Telefone</label>
-    <input type="number" name="telefone" id="telefone"  onChange={handleData} required/>
-    <hr/>
+    <label className="m-1" htmlFor="telefone">Telefone</label>
+    <input className="input-field  "type="number" name="telefone" id="telefone"  onChange={handleData} required/>
+
 
     
 
-    
-
-    <button type="submit" className=" m-2 border-2 border-slate-700 rounded-lg" onClick={submitUserData}>Cadastrar</button>
+    <button type="submit" className=" m-4 p-2 border-2 border-slate-200 text-slate-500 rounded-lg transition ease-linear hover:bg-teal-300 hover:scale-110 hover:shadow-lg" onClick={submitUserData}>Cadastrar</button>
   </div>
-  <div className="">
-    <p>Já tem uma conta? <a href="" className="border-2 line border-slate-500 rounded-lg px-1">Login</a>.</p>
+  <div className="m-2 text-slate-400 text-sm ">
+    <p>Já tem uma conta? <a href="" className="underline text-teal-300  rounded-lg p-1">Login</a>.</p>
   </div>
 </form> 
 
